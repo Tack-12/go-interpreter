e93dfb66e2e4f1cbc072cb4c70789736ae26a0e5
Pranaya Pudasaini <pranayauni1@gmail.com>
2026-05-09 02:18:49 -0500

    Minor error with lexer testing, decent progress

---
+++ lexer/lexer_test.go
@@ -0,0 +1,40 @@
+package lexer
+
+import (
+	"go-interpreter/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+{}(),;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.ADD, "+"},
+		{token.LBRAC, "{"},
+		{token.RBRAC, "}"},
+		{token.LPARA, "("},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+
+	for i ; tt := range tests{
+		tok:= l.NextToken()
+		
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - Token type is wrong expected=%q, got=%q", i , tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal!= tt.expectedLiteral{
+			t.Fatalf("test[%d] - Token type is wrong expected=%q, got=%q", i , tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
