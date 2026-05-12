9bbaaaa5465f843feedcdcc0cd4aff853ef0c1b5
Tack-12 <pranayauni1@gmail.com>
2026-05-12 13:33:48 -0500

    Progress so far , added error outputs

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"go-interpreter/ast"
 	"go-interpreter/lexer"
 	"go-interpreter/token"
@@ -11,16 +12,30 @@ type Parser struct {
 
 	currToken token.Token
 	peekToken token.Token
+
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l,
+		errors: []string{},
+	}
 
 	//Gets both the curr and next tokens
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s , got %s instead", t, p.peekToken.Type)
+
+	p.errors = append(p.errors, msg)
 }
 
 // Method to check the curr token and peek the at the nextToken
@@ -86,6 +101,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
