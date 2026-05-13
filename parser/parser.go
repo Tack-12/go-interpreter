c24cd9592dc76b1326245b6d87addcba441013b1
Tack-12 <pranayauni1@gmail.com>
2026-05-12 23:56:36 -0500

    infix and prefix parser fn not working

--- parser/parser.go
+++ parser/parser.go
@@ -14,6 +14,9 @@ type Parser struct {
 	peekToken token.Token
 
 	errors []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -121,4 +124,14 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+// Implementing Pratt Parser:
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -99,3 +99,35 @@ func TestReturnStatements(t *testing.T) {
 		}
 	}
 }
+
+func TestIdentifierExpression(t *testing.T) {
+
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("Program doesn't have enough statements. got=%T", program.Statements)
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statement[0] is not astStatement. got=%T ", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+
+	if !ok {
+		t.Fatalf("program.Statement[0] is not a Identifier. got=%T", ident.Value)
+	}
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value is not %s . got =%s", "foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.Value is not %s . got =%s", "foobar", ident.TokenLiteral())
+	}
+
+}
