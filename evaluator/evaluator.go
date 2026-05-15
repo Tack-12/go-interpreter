f2c64936d109a837d2b3bf6db0dbdbdd1f1cd340
Tack-12 <pranayauni1@gmail.com>
2026-05-15 15:01:26 -0500

    Added support for null

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"go-interpreter/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 
 	switch node := node.(type) {
@@ -19,7 +25,7 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return nativeBoolToBooleanObj(node.Value)
 	}
 
 	return nil
@@ -34,4 +40,13 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBooleanObj(input bool) *object.Boolean {
+
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
