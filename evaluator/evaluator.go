4bb605bff05e6a34e9b1be4312d673e415155395
Tack-12 <pranayauni1@gmail.com>
2026-05-15 14:36:11 -0500

    Integers works, added evaluation for integer with type int64

---
+++ evaluator/evaluator.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"go-interpreter/ast"
+	"go-interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+
+	var result object.Object
+
+	for _, statements := range stmts {
+		result = Eval(statements)
+	}
+
+	return result
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,50 @@
+package evaluator
+
+import (
+	"go-interpreter/lexer"
+	"go-interpreter/object"
+	"go-interpreter/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+
+	result, ok := obj.(*object.Integer)
+
+	if !ok {
+		t.Errorf("object has wrong value is not Integer. got=%T , of %V", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("The values and expected values do not match , want : %d, got :%d", expected, result.Value)
+		return false
+	}
+
+	return true
+}
