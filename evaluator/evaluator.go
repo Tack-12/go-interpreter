f3487b4062289ee7e1e1112fb199412f5c95f32d
Tack-12 <pranayauni1@gmail.com>
2026-05-16 15:55:46 -0500

    Added Error Handling

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"go-interpreter/ast"
 	"go-interpreter/object"
 )
@@ -57,8 +58,14 @@ func evalProgram(program *ast.Program) object.Object {
 
 	for _, statements := range program.Statements {
 		result = Eval(statements)
-		if returnValue, ok := result.(*object.ReturnValue); ok {
-			return returnValue.Value
+
+		switch result := result.(type) {
+
+		case *object.ReturnValue:
+			return result.Value
+
+		case *object.Error:
+			return result
 		}
 	}
 
@@ -69,8 +76,11 @@ func evalBlockStatement(block *ast.BlockStatement) object.Object {
 	var result object.Object
 	for _, statement := range block.Statements {
 		result = Eval(statement)
-		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
-			return result
+		if result != nil {
+			rt := result.Type()
+			if rt == object.RETURN_VALUE_OBJ || rt == object.ERROR_OBJ {
+				return result
+			}
 		}
 	}
 	return result
@@ -93,7 +103,7 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	case "-":
 		return evalMinusPrefixOperatorExpression(right)
 	default:
-		return nil
+		return newError("unknown operator: %s %s", operator, right.Type())
 	}
 
 }
@@ -115,7 +125,7 @@ func evalBangOperatorRight(right object.Object) object.Object {
 func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	if right.Type() != object.INTEGER_OBJ {
-		return nil
+		return newError("unknown operator: -%s", right.Type())
 	}
 
 	value := right.(*object.Integer).Value
@@ -131,8 +141,10 @@ func evalInfixExpression(op string, left, right object.Object) object.Object {
 		return nativeBoolToBooleanObj(left == right)
 	case op == "!=":
 		return nativeBoolToBooleanObj(left != right)
+	case left.Type() != right.Type():
+		return newError("type mismatch: %s %s %s", left.Type(), op, right.Type())
 	default:
-		return NULL
+		return newError("unknown operator: %s %s %s", left.Type(), op, right.Type())
 	}
 }
 
@@ -185,4 +197,8 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+func newError(format string, a ...interface{}) *object.Error {
+	return &object.Error{Message: fmt.Sprintf(format, a...)}
 }
